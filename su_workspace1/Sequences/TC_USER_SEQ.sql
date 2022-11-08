d6d4972680de0546376e4cf53377c2fd3bb0f3cc
SumanthJakkam <sumanth927@gmail.com>
2022-11-08 18:21:08 +0530

    Success

---
+++ su_workspace1/Sequences/TC_USER_SEQ.sql
@@ -0,0 +1,6 @@
+--------------------------------------------------------
+--  DDL for Sequence TC_USER_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  TC_USER_SEQ  
+   MINVALUE 1 MAXVALUE 99999999999 INCREMENT BY 1 START WITH 100 NOCACHE NOCYCLE;
