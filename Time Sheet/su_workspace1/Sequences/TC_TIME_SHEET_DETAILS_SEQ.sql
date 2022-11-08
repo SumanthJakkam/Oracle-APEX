91727f515271d1001b9f0f360d7891bd127aa175
SumanthJakkam <117716879+SumanthJakkam@users.noreply.github.com>
2022-11-08 15:25:39 +0530

    Delete Time Sheet/su_workspace1 directory

--- Time Sheet/su_workspace1/Sequences/TC_TIME_SHEET_DETAILS_SEQ.sql
+++
@@ -1,5 +0,0 @@
---------------------------------------------------------
---  DDL for Sequence TC_TIME_SHEET_DETAILS_SEQ
---------------------------------------------------------
-
-   CREATE SEQUENCE  "APEX"."TC_TIME_SHEET_DETAILS_SEQ"  MINVALUE 1 MAXVALUE 99999999999 INCREMENT BY 1 START WITH 10003 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
