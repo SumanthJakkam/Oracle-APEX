305b12bc3d80877734a12aa08cb2967e3fc535f9
SumanthJakkam <117716879+SumanthJakkam@users.noreply.github.com>
2022-11-08 15:25:21 +0530

    Delete Time Sheet/su_workspace1 directory

--- Time Sheet/su_workspace1/Sequences/TC_USER_ROLES_SEQ.sql
+++
@@ -1,6 +0,0 @@
---------------------------------------------------------
---  DDL for Sequence TC_USER_ROLES_SEQ
---------------------------------------------------------
-
-   CREATE SEQUENCE TC_USER_ROLES_SEQ  
-   MAXVALUE 99999999999999999999999 INCREMENT BY 1 START WITH 100000 NOORDER  NOCYCLE;
