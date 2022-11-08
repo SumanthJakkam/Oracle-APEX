e5f40a8ba1f3d06df064d8b71a9be0e4db614019
SumanthJakkam <117716879+SumanthJakkam@users.noreply.github.com>
2022-11-08 17:26:59 +0530

    Delete Time Sheet/su_workspace1 directory

--- Time Sheet/su_workspace1/Sequences/TC_EMPLOYEE_SEQ.sql
+++
@@ -1,6 +0,0 @@
---------------------------------------------------------
---  DDL for Sequence TC_EMPLOYEE_SEQ
---------------------------------------------------------
-
-   CREATE SEQUENCE TC_EMPLOYEE_SEQ  
-   MINVALUE 1 MAXVALUE 9999999999999999999999 INCREMENT BY 1 START WITH 10000 NOCACHE NOCYCLE;
