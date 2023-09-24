f907c59661afd9da6cf72a181689b7a5faf754b8
SumanthJakkam <sumanth927@gmail.com>
2023-09-24 13:31:05 +0530

    SU Workspace 2

---
+++ SU_WORKSPACE2/Dev/Sequences/TC_EMPLOYEE_SEQ.sql
@@ -0,0 +1,5 @@
+--------------------------------------------------------
+--  DDL for Sequence TC_EMPLOYEE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "APEX"."TC_EMPLOYEE_SEQ"  MINVALUE 1 MAXVALUE 99999999999 INCREMENT BY 1 START WITH 103 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
